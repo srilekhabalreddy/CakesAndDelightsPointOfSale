4dd91d50c16bb8ee15aa5bad8f81b5678de23e6d
SharanChaitanyaReddy <beemreddychaitanya@gmail.com>
2025-07-28 22:16:23 -0500

    Form.1 created

--- CakesAndDelightsPos/Form1.cs
+++ CakesAndDelightsPos/Form1.cs
@@ -1,3 +1,6 @@
+using static CakesAndDelightsPos.Cake;
+using Microsoft.Data.SqlClient;
+
 namespace CakesAndDelightsPos
 {
     public partial class Form1 : Form
@@ -5,6 +8,54 @@ namespace CakesAndDelightsPos
         public Form1()
         {
             InitializeComponent();
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        { }
+        private List<Cake> GetCakesFromDatabase()
+        {
+            List<Cake> cakes = new List<Cake>();
+
+            string connectionString = "Server=localhost;Database=CakesAndDelights;Integrated Security=True;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("usp_GetAllCakes", conn);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Cake cake = new Cake()
+                    {
+                        CakeId = Convert.ToInt32(reader["CakeId"]),
+                        Name = reader["Name"].ToString(),
+                        Price = Convert.ToDecimal(reader["Price"]),
+                        Description = reader["Description"].ToString()
+                    };
+                    cakes.Add(cake);
+                }
+
+                reader.Close();
+            }
+
+            return cakes;
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            List<Cake> cakeList = GetCakesFromDatabase();
+            dataGridViewCakes.DataSource = cakeList;
+
         }
     }
 }
+
---
+++ CakesAndDelightsPos/cake.cs
@@ -0,0 +1,15 @@
+﻿namespace CakesAndDelightsPos
+{
+
+
+    public class Cake
+    {
+        public int CakeId { get; set; }
+        public string Name { get; set; }       // Cake Name
+        public decimal Price { get; set; }     // Price per cake
+        public string Description { get; set; }      // Short description
+        public Boolean IsAvailable { get; set; }   //availability
+    }
+
+}
+
